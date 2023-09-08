5257bae1fa92e492fb4eba5e4233fb5df2c25234
Tomasz Le Thanh <s21216@pjwstk.edu.pl>
2023-09-08 16:34:16 +0200

    Added Swagger

--- src/main/java/com/example/backend/config/WebSecurityConfig.java
+++ src/main/java/com/example/backend/config/WebSecurityConfig.java
@@ -10,10 +10,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class WebSecurityConfig {
 
+    private static final String[] SWAGGER_PATHS = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/swagger-ui/**"};
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(SWAGGER_PATHS).permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .httpBasic(withDefaults())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
 
